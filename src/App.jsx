7a63d45a931989ec7fabbb409892924454fc111b
ahamedzoha <ahamed.zoha@gmail.com>
2022-10-29 00:39:04 +0600

    Nav separated 🗺️

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,13 @@
 import { About, Home, Contact, Error404 } from "./Pages"
-import {
-  Routes,
-  Route,
-  Link,
-  useLocation,
-  Navigate,
-  Outlet,
-} from "react-router-dom"
+import { Routes, Route, Navigate, Outlet } from "react-router-dom"
+
 import { useAuth } from "./Contexts/AuthContext"
+import { Navbar } from "./Global"
 
 const App = () => {
-  const location = useLocation()
-
-  const links = [
-    {
-      to: "/",
-      name: "Home",
-      active: location.pathname === "/",
-    },
-    {
-      to: "/about",
-      name: "About",
-      active: location.pathname === "/about",
-    },
-    {
-      to: "/contact",
-      name: "Contact",
-      active: location.pathname === "/contact",
-    },
-  ]
   return (
     <>
-      <nav className="bg-slate-700 px-2 py-4 text-white text-lg">
-        <ul className="flex  space-x-3">
-          {links.map((link) => (
-            <li key={link.to}>
-              <Link
-                to={link.to}
-                className={`py-2 px-3 rounded-lg ${
-                  link.active ? "bg-slate-600" : "bg-slate-700"
-                }`}
-              >
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-
+      <Navbar />
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route element={<ProtectedRoutes />}>
